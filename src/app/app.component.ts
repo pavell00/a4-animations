fc6231af9224cd74b44bac44b0a04e82488606b6
pavell00 <mymmo88@gmail.com>
2017-07-05 16:28:55 +0300

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    
+   trigger('listAnimation', [
+
+     transition('* => *', [
+
+       query(':enter', style({ opacity: 0 }), {optional: true}),
+
+       query(':enter', stagger('300ms', [
+         animate('1s ease-in', keyframes([
+           style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
+           style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+           style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+         ]))
+       ]), {optional: true}),
+
+       query(':leave', stagger('300ms', [
+         animate('1s ease-in', keyframes([
+           style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+           style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+           style({opacity: 0, transform: 'translateY(-75px)', offset: 1}),
+         ]))
+       ]), {optional: true}),
+
+     ])
+
+   ]),
+
+   trigger('explainerAnim', [
+     transition('* => *', [
+       query('.col', style({ opacity: 0, transform: 'translateX(-40px)' })),
+
+       query('.col', stagger('500ms', [
+         animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+       ]))
+
+     ])
+   ])
+
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  items = [];
+
+  constructor(){
+    this.items = ['Hey this is an item', 'Here is another one', 'This is awesome'];
+  }
+
+  pushItem(){
+    this.items.push('Oh eah that is awesome');
+  }
+
+  removeItem(){
+    this.items.pop();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -11,8 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
